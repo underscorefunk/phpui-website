b69c4c5de099b5f23ae86d83c51a4dad9fe43089
John Funk (Underscorefunk) <john@underscorefunk.com>
2022-11-04 14:28:04 -0500

    Made it cool

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     <head>
         <title>PHP UI</title>
     </head>
-    <body style="display: flex; justify-content: center; align-items: center">
+    <body style="color: #CCC; display: flex; justify-content: center; align-items: center">
         <h1>PHP UI</h1>
-<p>Underscorefunk Design</p>
+        <p>Underscorefunk Design</p>
     </body>
 </html>
